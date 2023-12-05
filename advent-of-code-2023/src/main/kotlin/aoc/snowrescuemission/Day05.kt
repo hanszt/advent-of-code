4a662cda6b68f9954e3124c75e0ecab7751905df
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2023-12-05 23:33:22 +0100

    Added day05test.txt

--- advent-of-code-2023/src/main/kotlin/aoc/snowrescuemission/Day05.kt
+++ advent-of-code-2023/src/main/kotlin/aoc/snowrescuemission/Day05.kt
@@ -10,8 +10,9 @@ class Day05(
 
     override fun part1(): Long {
         val input = text.split("\n\n")
-        val seeds = input[0].substring("seeds: ".length).splitToSequence(' ').map(String::toLong)
-        return toMinimumLocation(seeds, input)
+        return input[0].substring("seeds: ".length).splitToSequence(' ')
+            .map(String::toLong)
+            .toMinimumLocation(input)
     }
 
     override fun part2(): Long {
@@ -20,23 +21,25 @@ class Day05(
             .map(String::toLong)
             .chunked(2)
             .flatMap { (start, length) -> start..(start + length) }
-        return toMinimumLocation(seeds, input)
+        return seeds.toMinimumLocation(input)
     }
 
-    private fun toMinimumLocation(seeds: Sequence<Long>, input: List<String>): Long = seeds
-        .convert(input[1])
-        .convert(input[2])
-        .convert(input[3])
-        .convert(input[4])
-        .convert(input[5])
-        .convert(input[6])
-        .convert(input[7])
-        .min()
+    private fun Sequence<Long>.toMinimumLocation(input: List<String>): Long {
+
+        return convert(input[1])
+            .convert(input[2])
+            .convert(input[3])
+            .convert(input[4])
+            .convert(input[5])
+            .convert(input[6])
+            .convert(input[7])
+            .min()
+    }
 
     private fun Sequence<Long>.convert(line: String): Sequence<Long> {
-        val maps = line.lines().drop(1) // drop title
+        val mappings = line.lines().drop(1) // drop title
         val map = mutableMapOf<LongRange, LongRange>()
-        for (table in maps) {
+        for (table in mappings) {
             val (destStart, sourceStart, length) = table.split(' ').map(String::toLong)
             val sourceRange = sourceStart..(sourceStart + length)
             val destinationRange = destStart..(destStart + length)
--- advent-of-code-2023/src/test/kotlin/aoc/snowrescuemission/Day05Test.kt
+++ advent-of-code-2023/src/test/kotlin/aoc/snowrescuemission/Day05Test.kt
@@ -11,6 +11,11 @@ class Day05Test {
     }
 
     @Test
+    fun testPart2Test() {
+        Day05("input/day05test.txt").part2() shouldBe 46
+    }
+
+    @Test
     fun testPart2() {
         Day05("input/day05.txt").part2() shouldBe 0
     }
