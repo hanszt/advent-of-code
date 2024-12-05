11c2c55b92ed2d57fc8e70f962867eb7b128bae5
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2024-12-05 13:13:29 +0100

    Solved day 5

--- advent-of-code-2024/src/main/kotlin/aoc/historianhysteria/Day05.kt
+++ advent-of-code-2024/src/main/kotlin/aoc/historianhysteria/Day05.kt
@@ -1,25 +1,65 @@
 package aoc.historianhysteria
 
 import aoc.utils.ChallengeDay
-import aoc.utils.model.GridPoint2D.Companion.northeast
-import aoc.utils.model.GridPoint2D.Companion.northwest
-import aoc.utils.model.GridPoint2D.Companion.southeast
-import aoc.utils.model.GridPoint2D.Companion.southwest
+import aoc.utils.splitByBlankLine
 import java.nio.file.Path
 import kotlin.io.path.readText
 
-class Day05(private val input: String) : ChallengeDay {
+class Day05(input: String) : ChallengeDay {
+
+    private val orderingRules: Map<Int, MutableSet<Int>>
+    private val updates: List<List<Int>>
+
+    init {
+        val (orderingRules, updates) = input.splitByBlankLine()
+        this.orderingRules = buildMap {
+            orderingRules.lineSequence()
+                .map { it.split('|').let { it[0].toInt() to it[1].toInt() } }
+                .forEach { (left, add) ->
+                    computeIfAbsent(left) { HashSet() }.add(add)
+                }
+        }
+        this.updates = updates.lineSequence()
+            .map { it.splitToSequence(',').map(String::toInt).toList() }
+            .toList()
+    }
+
     constructor(path: Path) : this(path.readText())
 
-    private companion object {
-        val diagDirs = listOf(northwest, northeast, southeast, southwest)
+    override fun part1(): Int = updates(correct = true).sumOf(::middle)
+
+    override fun part2(): Int = updates(correct = false)
+        .map(::correct)
+        .sumOf(::middle)
+
+    private fun middle(ints: List<Int>): Int = ints[ints.size / 2]
+
+    private fun updates(correct: Boolean): Sequence<List<Int>> = sequence {
+        for (update in updates) {
+            var valid = true
+            outer@ for ((i, entry) in update.withIndex()) {
+                val requiredAfter = orderingRules[entry] ?: emptyList()
+                for (e in requiredAfter) {
+                    val j = update.indexOf(e)
+                    if (j in 0..<i) {
+                        valid = false
+                        break@outer
+                    }
+                }
+            }
+            if (valid == correct) {
+                yield(update)
+            }
+        }
     }
 
-    override fun part1(): Int {
-       TODO()
-    }
+    private fun correct(ints: List<Int>): List<Int> = ints.asSequence()
+        .sortedWith(::compareByOrderingRules)
+        .toList()
 
-    override fun part2(): Int {
-        TODO()
-    }
+    /**
+     * Shift the first element to the left if the second element should be after the first element.
+     */
+    private fun compareByOrderingRules(i1: Int, i2: Int): Int =
+        if (orderingRules[i1]?.contains(i2) == true) -1 else 0
 }
--- advent-of-code-2024/src/test/kotlin/aoc/historianhysteria/Day05Test.kt
+++ advent-of-code-2024/src/test/kotlin/aoc/historianhysteria/Day05Test.kt
@@ -10,41 +10,66 @@ class Day05Test {
     private companion object {
         private val day05 by lazy { Day05(Path("input/day05.txt")) }
         private val day05dr by lazy { Day05(Path("input/day05-dr.txt")) }
-        private val testInput = """
-            
-        """.trimIndent()
+        private val day05testInput = Day05("""
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+        """.trimIndent())
     }
 
     @Test
     fun part1TestInput() {
-        Day05(testInput).part1() shouldBe 18
+        day05testInput.part1() shouldBe 143
     }
 
     @Test
     fun part2TestInput() {
-        Day05(testInput).part2() shouldBe 9
+        day05testInput.part2() shouldBe 123
     }
 
     @Test
     fun part1dr() {
-        day05dr.part1() shouldBe 2599
+        day05dr.part1() shouldBe 5452
     }
 
     @Test
     fun part2dr() {
-        day05dr.part2() shouldBe 1948
+        day05dr.part2() shouldBe 4598
     }
 
     @Test
     fun part1() {
-        val expected = 2662
-        day05.part1() shouldBe expected
+        day05.part1() shouldBe 5091
     }
 
     @Test
     fun part2() {
-        val expected = 2034
-        day05.part2() shouldBe expected
+        day05.part2() shouldBe 4681
     }
 
 }
