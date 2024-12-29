a6d233131626882bf758fbf31154444570bc57b4
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2024-12-29 19:07:55 +0100

    Cleaned up day 24

--- advent-of-code-2024/src/main/kotlin/aoc/historianhysteria/Day24Zebalu.kt
+++ advent-of-code-2024/src/main/kotlin/aoc/historianhysteria/Day24Zebalu.kt
@@ -1,92 +1,75 @@
 package aoc.historianhysteria
 
+import aoc.utils.invoke
 import aoc.utils.parts
-import java.util.*
-import java.util.List
-import java.util.Map
-import java.util.function.Function
-import java.util.function.IntBinaryOperator
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-import java.util.stream.Collectors
 
-class Day24Zebalu(input: kotlin.collections.List<String>) {
+class Day24Zebalu(input: List<String>) {
     private val values: MutableMap<String, Int> = HashMap<String, Int>()
-    private val rules: MutableList<Rule>
+    private val rules: List<Rule>
     private val bitMap: MutableMap<Char, MutableList<String>> = HashMap<Char, MutableList<String>>()
 
     init {
         val groups = input.parts { it }
         for (initial in groups.first()) {
-            val parts: Array<String> = initial.split(": ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val parts = initial.split(": ")
             values.put(parts[0], parts[1].toInt())
-            if (BIT_PATTERN.matcher(parts[0]).matches()) {
+            if (BIT_PATTERN.matches(parts[0])) {
                 addToBitMap(parts[0])
             }
         }
-        rules = groups.last().stream().map<Rule> { input: String -> Rule.Companion.parse(input) }.toList()
-        rules.stream().filter { r: Rule -> r.result.startsWith("z") }.forEach { r: Rule -> addToBitMap(r.result) }
-        require(bitMap.get('x')!!.size == bitMap.get('y')!!.size) { "Invalid input! We should have as many Xes as many Ys!" }
+        rules = groups.last().map(Rule::parse)
+        rules.filter { it.result.startsWith("z") }.forEach { addToBitMap(it.result) }
+        require(bitMap('x').size == bitMap('y').size) { "Invalid input! We should have as many Xes as many Ys!" }
     }
 
-    fun part1(): Long {
-        return execute(HashMap<String, Int>(values), rules).toLong(2)
-    }
+    fun part1(): Long = execute(HashMap(values), rules).toLong(radix = 2)
 
     fun part2(): String {
-        val allBits = rules.stream().map<MutableList<String>> { r: Rule -> List.of<String>(r.first, r.second, r.result) }
-            .flatMap<String> { obj: MutableList<String> -> obj.stream() }.filter { str: String ->
-                this.isBit(
-                    str
-                )
-            }.collect(Collectors.toSet())
-        val xs = allBits.stream().filter { s: String -> s.startsWith("x") }.toList()
-        val zs = allBits.stream().filter { s: String -> s.startsWith("z") }.toList()
+        val allBits = rules.asSequence()
+            .flatMap { listOf(it.first, it.second, it.result) }
+            .filter(::isBit)
+            .toSet()
+        val xs = allBits.filter { it.startsWith("x") }
+        val zs = allBits.filter { it.startsWith("z") }
         val swapped: MutableList<String> = ArrayList<String>()
         var carry: String? = null
         for (i in xs.indices) {
             val xKey = String.format("x%02d", i)
             val yKey = String.format("y%02d", i)
             var outputs = fullAdder(xKey, yKey, carry, rules, swapped)
-            if (outputs.last() != null && outputs.last().startsWith("z") && (outputs.last() != "z" + (zs.size - 1))) {
-                outputs = outputs.reversed().toMutableList()
+            if (outputs.last().startsWith("z") && (outputs.last() != "z" + (zs.size - 1))) {
+                outputs = outputs.reversed()
                 swapped.add(outputs.first())
                 swapped.add(outputs.last())
             }
-            if (outputs.last() != null) {
-                carry = outputs.last()
-            } else {
-                carry = findMatchingRule(xKey, yKey, Rule.Companion.AND, rules).map<String>(Function { r: Rule -> r.result }).orElse(null)
-            }
+            carry = outputs.last()
         }
-        return swapped.stream().sorted().collect(Collectors.joining(","))
+        return swapped.sorted().joinToString(",")
     }
 
     private fun addToBitMap(key: String) {
         bitMap.compute(key[0]) { _, v ->
             if (v == null) {
-                return@compute ArrayList<String>(List.of<String>(key))
+                ArrayList<String>(listOf(key))
             } else {
-                if (Collections.binarySearch<String>(v, key) < 0) {
+                if (v.binarySearch(key) < 0) {
                     v.add(key)
-                    Collections.sort<String>(v)
+                    v.sort()
                 }
-                return@compute v
+                v
             }
         }
     }
 
 
-    private fun isBit(str: String): Boolean {
-        return BIT_PATTERN.matcher(str).matches()
-    }
+    private fun isBit(str: String): Boolean = BIT_PATTERN.matches(str)
 
-    private fun execute(copyOfValues: MutableMap<String, Int>, rules: MutableList<Rule>): String {
+    private fun execute(copyOfValues: MutableMap<String, Int>, rules: List<Rule>): String {
         var changed = true
-        val copy: MutableList<Rule> = LinkedList<Rule>(rules)
+        val copy = ArrayList<Rule>(rules)
         while (!copy.isEmpty() && changed) {
             changed = false
-            val it: MutableIterator<Rule> = copy.iterator()
+            val it = copy.iterator()
             while (it.hasNext()) {
                 val next = it.next()
                 if (next.canBeCalculated(copyOfValues)) {
@@ -96,26 +79,25 @@ class Day24Zebalu(input: kotlin.collections.List<String>) {
                 }
             }
         }
-        if (!copy.isEmpty()) {
-            return "1".repeat(63)
+        return if (copy.isNotEmpty()) {
+            "1".repeat(63)
         } else {
-            return copyOfValues.entries.stream()
-                .filter { e: MutableMap.MutableEntry<String, Int> -> e.key.startsWith("z") }
-                .sorted(KEY_COMPARATOR)
-                .map<Int> { it.value }
-                .map<String> { i: Int -> i.toString() }
-                .collect(Collectors.joining(""))
+            copyOfValues.entries.asSequence()
+                .filter { it.key.startsWith("z") }
+                .sortedByDescending { it.key }
+                .joinToString("") { it.value.toString() }
         }
     }
 
     @JvmRecord
-    data class Rule(val first: String, val second: String, val result: String, val operator: IntBinaryOperator) {
-        fun canBeCalculated(values: MutableMap<String, Int>): Boolean {
-            return values.containsKey(this.first) && values.containsKey(this.second) && !values.containsKey(this.result)
-        }
+    data class Rule(val first: String, val second: String, val result: String, val operator: (Int, Int) -> Int) {
+
+        fun canBeCalculated(values: Map<String, Int>): Boolean = values.containsKey(this.first) &&
+                values.containsKey(this.second) &&
+                !values.containsKey(this.result)
 
         fun calculate(values: MutableMap<String, Int>) {
-            values.put(result, this.operator.applyAsInt(values.get(first)!!, values.get(second)!!))
+            values.put(result, this.operator(values(first), values(second)))
         }
 
         val isTop: Boolean
@@ -129,7 +111,7 @@ class Day24Zebalu(input: kotlin.collections.List<String>) {
                 operator === AND -> result.append("AND")
                 operator === OR -> result.append("OR")
                 operator === XOR -> result.append("XOR")
-                else -> throw IllegalStateException("Unexpected value: " + operator)
+                else -> throw IllegalStateException("Unexpected value: $operator")
             }
             result.append(" ")
             result.append(second)
@@ -139,26 +121,20 @@ class Day24Zebalu(input: kotlin.collections.List<String>) {
         }
 
         companion object {
-            private val RULE_PATTERN: Pattern = Pattern.compile("(\\w+) (XOR|AND|OR) (\\w+) -> (\\w+)")
-            internal val AND = IntBinaryOperator { a: Int, b: Int -> if (a == 1 && b == 1) 1 else 0 }
-            internal val OR = IntBinaryOperator { a: Int, b: Int -> if (a == 1 || b == 1) 1 else 0 }
-            internal val XOR = IntBinaryOperator { a: Int, b: Int -> if (a != b) 1 else 0 }
+            private val RULE_PATTERN = Regex("(\\w+) (XOR|AND|OR) (\\w+) -> (\\w+)")
+            internal val AND = { a: Int, b: Int -> if (a == 1 && b == 1) 1 else 0 }
+            internal val OR = { a: Int, b: Int -> if (a == 1 || b == 1) 1 else 0 }
+            internal val XOR = { a: Int, b: Int -> if (a != b) 1 else 0 }
 
             fun parse(input: String): Rule {
-                val matcher: Matcher = RULE_PATTERN.matcher(input)
-                if (matcher.matches()) {
-                    val first = matcher.group(1)
-                    val second = matcher.group(3)
-                    val result = matcher.group(4)
-                    val op = matcher.group(2)
-                    return when (op) {
-                        "XOR" -> Rule(first, second, result, XOR)
-                        "AND" -> Rule(first, second, result, AND)
-                        "OR" -> Rule(first, second, result, OR)
-                        else -> throw IllegalStateException("Unexpected value: " + op + " in line: " + input)
-                    }
+                val (first, op, second, result) = RULE_PATTERN.matchEntire(input)
+                    ?.destructured ?: error("Unexpected input: $input")
+                return when (op) {
+                    "XOR" -> Rule(first, second, result, XOR)
+                    "AND" -> Rule(first, second, result, AND)
+                    "OR" -> Rule(first, second, result, OR)
+                    else -> throw IllegalStateException("Unexpected value: $op in line: $input")
                 }
-                throw IllegalStateException("Unexpected input: " + input)
             }
         }
     }
@@ -188,67 +164,67 @@ class Day24Zebalu(input: kotlin.collections.List<String>) {
      *
      * [Original Idea](https://github.com/xhyrom/aoc/blob/main/2024/24/part_2.py)
      * All hail to @xhyrom */
-    fun fullAdder(a: String, b: String, carry: String?, rules: MutableList<Rule>, swapped: MutableList<String>): MutableList<String> {
-        var basicSum = findMatchingRule(a, b, Rule.Companion.XOR, rules)
-        basicSum.orElseThrow()
-        var basicCarry = findMatchingRule(a, b, Rule.Companion.AND, rules)
-        basicCarry.orElseThrow()
+    fun fullAdder(
+        a: String,
+        b: String,
+        carry: String?,
+        rules: List<Rule>,
+        swapped: MutableList<String>
+    ): List<String> {
+        var basicSum = findMatchingRule(a, b, Rule.XOR, rules) ?: error("!")
+        var basicCarry = findMatchingRule(a, b, Rule.AND, rules) ?: error("!")
         if (carry == null) {
-            return List.of<String>(basicSum.get().result, basicCarry.get().result)
+            return listOf(basicSum.result, basicCarry.result)
         } else {
-            var carryForward1 = findMatchingRule(basicSum.get().result, carry, Rule.Companion.AND, rules)
-            if (carryForward1.isEmpty()) {
-                swapped.add(basicSum.get().result)
-                swapped.add(basicCarry.get().result)
+            var carryForward1 = findMatchingRule(basicSum.result, carry, Rule.AND, rules)
+            if (carryForward1 == null) {
+                swapped.add(basicSum.result)
+                swapped.add(basicCarry.result)
                 val temp = basicSum
                 basicSum = basicCarry
                 basicCarry = temp
-                carryForward1 = findMatchingRule(basicSum.get().result, carry, Rule.Companion.AND, rules)
+                carryForward1 = findMatchingRule(basicSum.result, carry, Rule.AND, rules)
             }
 
-            var finalSum = findMatchingRule(basicSum.get().result, carry, Rule.Companion.XOR, rules)
+            var finalSum = findMatchingRule(basicSum.result, carry, Rule.XOR, rules)
 
-            if (basicSum.get().isTop && finalSum.isPresent()) {
+            if (basicSum.isTop && finalSum != null) {
                 val temp = basicSum
                 basicSum = finalSum
                 finalSum = temp
-                swapped.add(finalSum.get().result)
-                swapped.add(basicSum.get().result)
+                swapped.add(finalSum.result)
+                swapped.add(basicSum.result)
             }
-            if (basicCarry.get().isTop && finalSum.isPresent()) {
+            if (basicCarry.isTop && finalSum != null) {
                 val temp = basicCarry
                 basicCarry = finalSum
                 finalSum = temp
-                swapped.add(basicCarry.get().result)
-                swapped.add(finalSum.get().result)
+                swapped.add(basicCarry.result)
+                swapped.add(finalSum.result)
             }
-            if (carryForward1.isPresent() && carryForward1.get().isTop && finalSum.isPresent()) {
+            if (carryForward1 != null && carryForward1.isTop && finalSum != null) {
                 val temp = carryForward1
                 carryForward1 = finalSum
                 finalSum = temp
-                swapped.add(carryForward1.get().result)
-                swapped.add(finalSum.get().result)
+                swapped.add(carryForward1.result)
+                swapped.add(finalSum.result)
             }
-            val finalCarry: Optional<Rule>
-            if (carryForward1.isPresent()) {
-                finalCarry = findMatchingRule(carryForward1.get().result, basicCarry.get().result, Rule.Companion.OR, rules)
-            } else {
-                finalCarry = Optional.empty<Rule>()
-            }
-            val result: MutableList<String> = ArrayList<String>()
-            result.add(finalSum.map<String>(Rule::result).orElse(null))
-            result.add(finalCarry.map<String>(Rule::result).orElse(null))
+            val finalCarry = if (carryForward1 != null) {
+                findMatchingRule(carryForward1.result, basicCarry.result, Rule.OR, rules)
+            } else null
+            val result = ArrayList<String>()
+            finalSum?.result?.also { result.add(it) }
+            finalCarry?.result?.also { result.add(it) }
             return result
         }
     }
 
-    fun findMatchingRule(a: String, b: String, operator: IntBinaryOperator, rules: MutableList<Rule>): Optional<Rule> {
-        return rules.stream().filter { r: Rule -> r.operator === operator }
-            .filter { r: Rule -> (r.first == a && r.second == b) || (r.second == a && r.first == b) }.findFirst()
-    }
+    fun findMatchingRule(a: String, b: String, operator: (Int, Int) -> Int, rules: List<Rule>): Rule? = rules.asSequence()
+        .filter { it.operator === operator }
+        .filter { (it.first == a && it.second == b) || (it.second == a && it.first == b) }
+        .firstOrNull()
 
     companion object {
-        private val BIT_PATTERN: Pattern = Pattern.compile("[xyz]\\d+")
-        private val KEY_COMPARATOR: Comparator<MutableMap.MutableEntry<String, Int>> = Map.Entry.comparingByKey<String, Int>().reversed()
+        private val BIT_PATTERN = Regex("[xyz]\\d+")
     }
 }
