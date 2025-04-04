6b4b7b64a94187326c0257ead964e84e2cc222eb
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2025-04-04 11:12:31 +0200

    tests running utils

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,12 @@
  */
 
 rootProject.name = "advent-of-code"
-include(":advent-of-code-2021")
-include(":advent-of-code-2020")
-include(":advent-of-code-2023")
-include(":advent-of-code-2022")
+//include(":advent-of-code-2021")
+//include(":advent-of-code-2020")
+//include(":advent-of-code-2023")
+//include(":advent-of-code-2022")
 include(":aoc-utils")
-include(":downloader")
-include(":advent-of-code-2019")
-include(":advent-of-code-2024")
-include(":aoc-runner")
+//include(":downloader")
+//include(":advent-of-code-2019")
+//include(":advent-of-code-2024")
+//include(":aoc-runner")
