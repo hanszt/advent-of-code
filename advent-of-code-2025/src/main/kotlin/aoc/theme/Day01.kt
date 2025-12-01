2b36c940c32ef2c174adc18ee736eae33796089b
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2025-12-01 18:03:24 +0100

    Solve day 1

--- advent-of-code-2025/src/main/kotlin/aoc/theme/Day01.kt
+++ advent-of-code-2025/src/main/kotlin/aoc/theme/Day01.kt
@@ -3,27 +3,48 @@ package aoc.theme
 import aoc.utils.ChallengeDay
 import java.nio.file.Path
 import kotlin.io.path.readLines
-import kotlin.math.abs
 
 class Day01(private val lines: List<String>) : ChallengeDay {
     constructor(path: Path) : this(path.readLines())
 
     override fun part1(): Int {
-        val (l1, l2) = lines.asSequence()
-            .map { line -> line.split("   ").let { it[0].toInt() to it[1].toInt() } }
-            .unzip()
-        val l1Sorted = l1.sorted()
-        val l2Sorted = l2.sorted()
-
-        return l1Sorted.zip(l2Sorted).sumOf { (n1, n2) -> abs(n1 - n2) }
+        var timesAtZero = 0
+        var dial = 50
+        for (line in lines) {
+            val amount = line.drop(1).toInt()
+            dial = when (line.first()) {
+                'R' -> dial + amount
+                'L' -> dial - amount
+                else -> error("Invalid direction: $line")
+            } % 100
+            if (dial == 0) timesAtZero++
+        }
+        return timesAtZero
     }
 
+    /**
+     * Calculates the number of times a position wraps https://github.com/elizarov/AdventOfCode2025/blob/main/src/Day01.kt
+     */
     override fun part2(): Int {
-        val (l1, l2) = lines.asSequence()
-            .map { line -> line.split("   ").let { it[0].toInt() to it[1].toInt() } }
-            .unzip()
-        val eachCount = l2.groupingBy { it }.eachCount()
-        return l1.sumOf { it * (eachCount[it] ?: 0) }
+        var cnt = 0
+        var pos = 50
+        for (line in lines) {
+            val amount = line.substring(1).toInt()
+            when (line[0]) {
+                'R' -> {
+                    pos += amount
+                    cnt += pos / 100
+                    pos = pos.mod(100)
+                }
 
+                'L' -> {
+                    if (pos == 0) cnt--
+                    pos -= amount
+                    cnt += (-pos + 100) / 100
+                    pos = pos.mod(100)
+                }
+            }
+        }
+        return cnt
     }
 }
--- advent-of-code-2025/src/test/kotlin/aoc/theme/Day01Test.kt
+++ advent-of-code-2025/src/test/kotlin/aoc/theme/Day01Test.kt
@@ -8,46 +8,34 @@ class Day01Test {
 
     private companion object {
         private val day01 = Day01(Path("input/day01.txt"))
-        private val day01Dr = Day01(Path("input/day01-dr.txt"))
-        private val day01Test = Day01(
-            lines = """
-                3   4
-                4   3
-                2   5
-                1   3
-                3   9
-                3   3
-            """.trimIndent().lines()
+        private val day01TestInput = Day01(
+            listOf(
+                "L68",
+                "L30",
+                "R48",
+                "L5",
+                "R60",
+                "L55",
+                "L1",
+                "L99",
+                "R14",
+                "L82",
+            )
         )
     }
 
     @Test
     fun `test part 1`() {
-        day01.part1() shouldBe 2_086_478
+        day01.part1() shouldBe 1180
     }
 
     @Test
-    fun `test part 1 v2`() {
-        day01Dr.part1() shouldBe 2_904_518
-    }
-
-    @Test
-    fun `test part 1 test`() {
-        day01Test.part1() shouldBe 11
+    fun `test part 2 test input`() {
+        day01TestInput.part2() shouldBe 6
     }
 
     @Test
     fun `test part 2`() {
-        day01.part2() shouldBe 24_941_624
-    }
-
-    @Test
-    fun `test part 2 test`() {
-        day01Test.part2() shouldBe 31
-    }
-
-    @Test
-    fun `test part 2 v2`() {
-        day01Dr.part2() shouldBe 18_650_129
+        day01.part2() shouldBe 6892 // 5665, 6312 Too low // 6911 Too high
     }
 }
