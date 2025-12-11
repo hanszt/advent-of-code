c7346069bea729b7ad9e55fb33d70a0b6a993535
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2025-12-11 09:13:01 +0100

    Add day 11 and 12 templates

---
+++ advent-of-code-2025/src/main/kotlin/aoc/secret_entrance/Day11.kt
@@ -0,0 +1,16 @@
+package aoc.secret_entrance
+
+import aoc.utils.ChallengeDay
+import java.nio.file.Path
+import kotlin.io.path.readLines
+
+/**
+ *
+ */
+class Day11(private val lines: List<String>) : ChallengeDay {
+
+    constructor(path: Path) : this(path.readLines())
+
+    override fun part1(): Long = TODO()
+    override fun part2(): Int = TODO()
+}
