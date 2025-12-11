34555f80ef2e678e3c02592a5a8bdfc9bad169c2
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2025-12-11 13:56:21 +0100

    Make vars final

--- advent-of-code-2025/src/main/java/aoc/secret_entrance/Day02.java
+++ advent-of-code-2025/src/main/java/aoc/secret_entrance/Day02.java
@@ -11,7 +11,7 @@ import java.util.stream.LongStream;
 
 public record Day02(List<IdRange> idRanges) implements ChallengeDay {
 
-    public Day02(String idRanges) {
+    public Day02(final String idRanges) {
         this(Patterns.commaPattern.splitAsStream(idRanges)
                 .map(IdRange::parse)
                 .toList());
@@ -27,33 +27,33 @@ public record Day02(List<IdRange> idRanges) implements ChallengeDay {
         return invalidIdSum(Day02::isInvalidIdP2);
     }
 
-    private long invalidIdSum(LongPredicate isInvalidId) {
+    private long invalidIdSum(final LongPredicate isInvalidId) {
         return idRanges.stream()
                 .flatMapToLong(idRange -> {
-                    var min = Math.min(idRange.start, idRange.end);
-                    var max = Math.max(idRange.start, idRange.end);
+                    final var min = Math.min(idRange.start, idRange.end);
+                    final var max = Math.max(idRange.start, idRange.end);
                     return LongStream.rangeClosed(min, max).filter(isInvalidId);
                 }).sum();
     }
 
-    private static boolean isInvalidIdP1(long l) {
-        var s = String.valueOf(l);
+    private static boolean isInvalidIdP1(final long l) {
+        final var s = String.valueOf(l);
         if (s.length() % 2 == 0) {
-            var start = s.substring(0, s.length() / 2);
-            var end = s.substring(s.length() / 2);
+            final var start = s.substring(0, s.length() / 2);
+            final var end = s.substring(s.length() / 2);
             return start.equals(end);
         }
         return false;
     }
 
-    private static boolean isInvalidIdP2(long l) {
+    private static boolean isInvalidIdP2(final long l) {
         final var s = StringX.of(l);
         final var size = s.length();
         for (var chunkSize = 1; chunkSize <= size / 2; chunkSize++) {
             if (size % chunkSize != 0) {
                 continue;
             }
-            var isRepeating = s.codePointSequence()
+            final var isRepeating = s.codePointSequence()
                     .chunked(chunkSize)
                     .zipWithNext(Objects::equals)
                     .all(b -> b);
@@ -65,7 +65,7 @@ public record Day02(List<IdRange> idRanges) implements ChallengeDay {
     }
 
     record IdRange(long start, long end) {
-        static IdRange parse(String s) {
+        static IdRange parse(final String s) {
             final var split = s.split("-");
             return new IdRange(Long.parseLong(split[0]), Long.parseLong(split[1]));
         }
