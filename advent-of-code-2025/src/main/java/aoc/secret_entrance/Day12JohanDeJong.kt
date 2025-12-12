d9cebcef63d3234b92ecdc1fd3d90dc1082b3a0e
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2025-12-12 22:56:19 +0100

    Konverted

--- advent-of-code-2025/src/main/java/aoc/secret_entrance/Day12JohanDeJong.kt
+++ advent-of-code-2025/src/main/java/aoc/secret_entrance/Day12JohanDeJong.kt
@@ -1,262 +1,192 @@
-package aoc.secret_entrance;
+package aoc.secret_entrance
 
-import aoc.utils.grid2d.Grid2DUtilsKt;
-import aoc.utils.grid2d.GridPoint2D;
+import aoc.utils.grid2d.*
+import aoc.utils.parts
 
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+/** [Johan de Jong Day 12](https://github.com/johandj123/adventofcode2025/blob/master/src/Day12.java) */
+class Day12JohanDeJong(private val text: String) {
 
-/// [Johan de Jong Day 12](https://github.com/johandj123/adventofcode2025/blob/master/src/Day12.java)
-public record Day12JohanDeJong(String text) {
+    fun run(): Int {
+        val parts = text.lines().parts { it }
+        val presents = parts.dropLast(1).map { CharMatrix(it.drop(1)) }
+        val regions = parts.last().map(::Region)
+        val presentSizes = presents.map(::countGridPoint2Ds)
+        val presentsAllOrientations = presents.map(::allOrientations)
 
-    public int run() {
-        final var parts = text.split("\n\n");
-        final var presents = new ArrayList<CharMatrix>();
-        for (var i = 0; i < parts.length - 1; i++) {
-            var s = parts[i];
-            s = s.substring(s.indexOf('\n') + 1);
-            presents.add(new CharMatrix(s));
-        }
-        final var regions = Arrays.stream(parts[parts.length - 1].split("\n")).map(Region::new).toList();
-        final var presentSizes = presents.stream().map(Day12JohanDeJong::countGridPoint2Ds).toList();
-        final var presentsAllOrientations = presents.stream().map(Day12JohanDeJong::allOrientations).toList();
-
-        var count = 0;
-        for (final var region : regions) {
-            if (region.size() < region.countGridPoint2DsNeeded(presentSizes)) {
-                continue;
+        var count = 0
+        for (region in regions) {
+            if (region.size < region.countGridPoint2DsNeeded(presentSizes)) {
+                continue
             }
             if (region.canPlacePresents(presentsAllOrientations)) {
-                count++;
+                count++
             }
         }
-        return count;
+        return count
     }
 
-    private static Set<CharMatrix> allOrientations(final CharMatrix charMatrix) {
-        return reachable(charMatrix, c -> List.of(c, c.mirrorHorizontal(), c.mirrorVertical(), c.transpose()));
-    }
+    internal class Region(line: String) {
+        val width: Int
+        val height: Int
+        val counts: List<Int>
 
-    private static int countGridPoint2Ds(final CharMatrix charMatrix) {
-        return (int) charMatrix.chars().filter(c -> c == '#').count();
-    }
-
-    static class Region {
-        int width;
-        int height;
-        List<Integer> counts;
-
-        public Region(final String line) {
-            final var ints = extractPositiveIntegers(line);
-            width = ints.get(0);
-            height = ints.get(1);
-            counts = ints.subList(2, ints.size());
+        init {
+            val ints = extractPositiveIntegers(line)
+            width = ints[0]
+            height = ints[1]
+            counts = ints.drop(2)
         }
 
-        public static List<Integer> extractPositiveIntegers(final String input) {
-            final var parts = input.split("\\D");
-            return Arrays.stream(parts)
-                    .filter(s -> !s.isBlank())
-                    .map(Integer::parseInt)
-                    .toList();
-        }
+        val size: Int = width * height
 
-        int size() {
-            return width * height;
-        }
+        fun countGridPoint2DsNeeded(presentSizes: List<Int>): Int =
+            counts.indices.sumOf { counts[it] * presentSizes[it] }
 
-        int countGridPoint2DsNeeded(final List<Integer> presentSizes) {
-            var result = 0;
-            for (var i = 0; i < counts.size(); i++) {
-                result += (counts.get(i) * presentSizes.get(i));
-            }
-            return result;
-        }
-
-        List<Set<CharMatrix>> presentsToBePlaced(final List<Set<CharMatrix>> presentsAllOrientations) {
-            final var result = new ArrayList<Set<CharMatrix>>();
-            for (var i = 0; i < counts.size(); i++) {
-                for (var j = 0; j < counts.get(i); j++) {
-                    result.add(presentsAllOrientations.get(i));
+        fun presentsToBePlaced(presentsAllOrientations: List<Set<CharMatrix>>): List<Set<CharMatrix>> = buildList {
+            for (i in counts.indices) {
+                repeat(counts[i]) {
+                    add(presentsAllOrientations[i])
                 }
             }
-            return result;
         }
 
-        @Override
-        public String toString() {
-            return "Region{" +
-                    "width=" + width +
-                    ", height=" + height +
-                    ", counts=" + counts +
-                    '}';
+        override fun toString(): String = "Region{width=$width, height=$height, counts=$counts}"
+
+        fun canPlacePresents(presentsAllOrientations: List<Set<CharMatrix>>): Boolean {
+            val charMatrix = CharMatrix(height, width, '.')
+            val presentList = presentsToBePlaced(presentsAllOrientations)
+            return charMatrix.canPlacePresents(presentList)
         }
 
-        private boolean canPlacePresents(final List<Set<CharMatrix>> presentsAllOrientations) {
-            final var charMatrix = new CharMatrix(height, width, '.');
-            final var presentList = presentsToBePlaced(presentsAllOrientations);
-            return canPlacePresents(charMatrix, presentList);
-        }
-
-        private boolean canPlacePresents(final CharMatrix charMatrix, final List<Set<CharMatrix>> presentsAllOrientations) {
+        private fun CharMatrix.canPlacePresents(presentsAllOrientations: List<Set<CharMatrix>>): Boolean {
             if (presentsAllOrientations.isEmpty()) {
-                return true;
+                return true
             }
-            final var presentOrientations = presentsAllOrientations.getFirst();
-            for (final var present : presentOrientations) {
-                for (var y = 0; y < charMatrix.getHeight() - present.getHeight() + 1; y++) {
-                    for (var x = 0; x < charMatrix.getWidth() - present.getWidth() + 1; x++) {
-                        final var position = GridPoint2D.of(x, y);
-                        if (canPlace(charMatrix, present, position)) {
-                            place(charMatrix, present, position);
-                            final var canPlace = canPlacePresents(charMatrix, presentsAllOrientations.subList(1, presentsAllOrientations.size()));
+            val presentOrientations: Set<CharMatrix> = presentsAllOrientations.first()
+            for (present in presentOrientations) {
+                for (y in 0..<this.height - present.height + 1) {
+                    for (x in 0..<this.width - present.width + 1) {
+                        val position = GridPoint2D(x, y)
+                        if (canPlace(present, position)) {
+                            place(present, position)
+                            val canPlace = canPlacePresents(presentsAllOrientations.drop(1))
                             if (canPlace) {
-                                return true;
+                                return true
                             }
-                            unplace(charMatrix, present, position);
+                            unplace(present, position)
                         }
                     }
                 }
             }
-            return false;
+            return false
         }
 
-        private boolean canPlace(final CharMatrix charMatrix, final CharMatrix present, final GridPoint2D position) {
-            for (var y = 0; y < present.getHeight(); y++) {
-                for (var x = 0; x < present.getWidth(); x++) {
-                    if (present.get(x, y) == '#' && charMatrix.get(position.getX() + x, position.getY() + y) == '#') {
-                        return false;
+        private fun CharMatrix.canPlace(present: CharMatrix, position: GridPoint2D): Boolean {
+            for (y in 0..<present.height) {
+                for (x in 0..<present.width) {
+                    val d = GridPoint2D(x, y)
+                    if (present[d] == '#' && this[position + d] == '#') {
+                        return false
                     }
                 }
             }
-            return true;
+            return true
         }
 
-        private void place(final CharMatrix charMatrix, final CharMatrix present, final GridPoint2D position) {
-            set(present, charMatrix, position, '#');
+        private fun CharMatrix.place(present: CharMatrix, position: GridPoint2D) {
+            set(present, position, '#')
         }
 
-        private void unplace(final CharMatrix charMatrix, final CharMatrix present, final GridPoint2D position) {
-            set(present, charMatrix, position, '.');
+        private fun CharMatrix.unplace(present: CharMatrix, position: GridPoint2D) {
+            set(present, position, '.')
         }
 
-        private static void set(final CharMatrix present, final CharMatrix charMatrix, final GridPoint2D position, final char c) {
-            for (var y = 0; y < present.getHeight(); y++) {
-                for (var x = 0; x < present.getWidth(); x++) {
-                    if (present.get(x, y) == '#') {
-                        charMatrix.set(position.getX() + x, position.getY() + y, c);
+        companion object {
+            private val nonDigit = "\\D".toRegex()
+
+            fun extractPositiveIntegers(input: String): List<Int> {
+                return input.split(nonDigit).filter(String::isNotBlank).map(String::toInt)
+            }
+
+            private fun CharMatrix.set(present: CharMatrix, position: GridPoint2D, c: Char) {
+                for (y in 0..<present.height) {
+                    for (x in 0..<present.width) {
+                        val d = GridPoint2D(x, y)
+                        if (present[d] == '#') {
+                            this[position + d] = c
+                        }
                     }
                 }
             }
         }
     }
 
-    public static <T> Set<T> reachable(final T start, final Function<? super T, ? extends Iterable<T>> toNeighbours) {
-        final var startList = List.of(start);
-        final var seen = new HashSet<>(startList);
-        final var todo = new ArrayDeque<>(startList);
-        while (!todo.isEmpty()) {
-            final var node = todo.remove();
-            for (final var nextNode : toNeighbours.apply(node)) {
-                if (seen.add(nextNode)) {
-                    todo.add(nextNode);
-                }
+    internal class CharMatrix(private val content: Array<CharArray>) {
+
+        constructor(content: List<String>) : this(
+            Array(content.size) { y -> CharArray(content[y].length) { x -> content[y][x] } }
+        )
+
+        constructor(height: Int, width: Int, fill: Char) :
+                this(Array(height) { CharArray(width) { fill } })
+
+        fun chars(): Sequence<Char> = content.asSequence().flatMap { it.asSequence() }
+
+        val width: Int = content[0].size
+        val height: Int = content.size
+
+        operator fun get(p: GridPoint2D): Char = content[p]
+
+        operator fun set(p: GridPoint2D, c: Char) {
+            content[p] = c
+        }
+
+        fun getRow(y: Int): String = buildString(content[y].size) {
+            for (x in content[y].indices) {
+                append(content[y][x])
             }
         }
-        return seen;
+
+        fun getColumn(x: Int): String = buildString(content.size) {
+            for (chars in content) {
+                append(chars[x])
+            }
+        }
+
+        fun transpose(): CharMatrix {
+            val s = List(this.width) { getColumn(it) }
+            return CharMatrix(s)
+        }
+
+        fun mirrorHorizontal(): CharMatrix = CharMatrix(content.mirroredHorizontally())
+        fun mirrorVertical(): CharMatrix = CharMatrix(content.mirroredVertically())
+
+        override fun equals(other: Any?): Boolean =
+            this === other || (other is CharMatrix && content.contentDeepEquals(other.content))
+
+        override fun hashCode(): Int = content.contentDeepHashCode()
+        override fun toString(): String = content.indices.joinToString("\n", transform = this::getRow)
     }
 
-    record CharMatrix(char[][] content) {
+    companion object {
+        private fun allOrientations(charMatrix: CharMatrix): Set<CharMatrix> =
+            charMatrix.seenByBfs { listOf(it, it.mirrorHorizontal(), it.mirrorVertical(), it.transpose()) }
 
-        CharMatrix(final String[] content) {
-            this(new char[content.length][Arrays.stream(content).mapToInt(String::length).max().orElse(0)]);
-                for (var y = 0; y < content.length; y++) {
-                    for (var x = 0; x < content[y].length(); x++) {
-                        this.content[y][x] = content[y].charAt(x);
+        private fun countGridPoint2Ds(charMatrix: CharMatrix): Int = charMatrix.chars().count { it == '#' }
+
+        fun <T> T.seenByBfs(toNeighbours: (T) -> Iterable<T>): Set<T> {
+            val start = setOf(this)
+            val seen = HashSet(start)
+            val todo = ArrayDeque(start)
+            while (todo.isNotEmpty()) {
+                val node = todo.removeLast()
+                for (nextNode in toNeighbours(node)) {
+                    if (seen.add(nextNode)) {
+                        todo.add(nextNode)
                     }
                 }
             }
-
-            CharMatrix(final String content) {
-                this(content.split("\n"));
-            }
-
-            CharMatrix(final int height, final int width, final char fill) {
-                this(new char[height][width]);
-                for (var y = 0; y < height; y++) {
-                    Arrays.fill(this.content[y], fill);
-                }
-            }
-
-            IntStream chars() {
-                return Arrays.stream(content).flatMapToInt(s -> new String(s).chars());
-            }
-
-            int getWidth() {
-                return content[0].length;
-            }
-
-            int getHeight() {
-                return content.length;
-            }
-
-            char get(final int x, final int y) {
-                return content[y][x];
-            }
-
-            void set(final int x, final int y, final char c) {
-                content[y][x] = c;
-            }
-
-            String getRow(final int y) {
-                final var stringBuilder = new StringBuilder();
-                for (var x = 0; x < content[y].length; x++) {
-                    stringBuilder.append(content[y][x]);
-                }
-                return stringBuilder.toString();
-            }
-
-            String getColumn(final int x) {
-                final var stringBuilder = new StringBuilder();
-                for (char[] chars : content) {
-                    stringBuilder.append(chars[x]);
-                }
-                return stringBuilder.toString();
-            }
-
-            CharMatrix transpose() {
-                final var s = new String[getWidth()];
-                for (var i = 0; i < getWidth(); i++) {
-                    s[i] = getColumn(i);
-                }
-                return new CharMatrix(s);
-            }
-
-            CharMatrix mirrorHorizontal() {
-                return new CharMatrix(Grid2DUtilsKt.mirroredHorizontally(content));
-            }
-
-            CharMatrix mirrorVertical() {
-                return new CharMatrix(Grid2DUtilsKt.mirroredVertically(content));
-            }
-
-            @Override
-            public boolean equals(final Object o) {
-                return this == o || (o instanceof CharMatrix(char[][] other) && Arrays.deepEquals(content, other));
-            }
-
-            @Override
-            public int hashCode() {
-                return Arrays.deepHashCode(content);
-            }
-
-            @Override
-            public String toString() {
-                return IntStream.range(0, content.length)
-                        .mapToObj(this::getRow)
-                        .collect(Collectors.joining("\n"));
-            }
+            return seen
         }
+    }
 }
