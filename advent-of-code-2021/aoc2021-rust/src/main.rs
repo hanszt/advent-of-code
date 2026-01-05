f0b7a68c179c9007313bb1179c127cd56abc426a
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2026-01-05 18:40:29 +0100

    Add day15chiton.rs for 2021 challenge

--- advent-of-code-2021/aoc2021-rust/src/main.rs
+++ advent-of-code-2021/aoc2021-rust/src/main.rs
@@ -1,4 +1,4 @@
-use aoc2021_rust::{day01, day18snailfish};
+use aoc2021_rust::{day01, day15chiton, day18snailfish};
 use aoc2021_rust::{day02, day23amphipod};
 
 fn main() {
@@ -11,6 +11,11 @@ fn main() {
     let input_day2 = include_str!("../../input/day2.txt");
     println!("part 1: {}", day02::part_1(input_day2));
     println!("part 2: {}", day02::part_2(input_day2));
+
+    println!("\nDay 15 chiton");
+    let input_day15 = include_str!("../../input/day15.txt");
+    println!("part 1: {}", day15chiton::part_1(input_day15));
+    println!("part 2: {}", day15chiton::part_2(input_day15));
 
     println!("\nDay 18 Snailfish");
     let input_day18 = include_str!("../../input/day18.txt");
