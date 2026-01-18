dba06884743af6a1aa93268025cc17fb1b55e4cb
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2026-01-18 17:51:45 +0100

    Renaming to Cube

--- advent-of-code-2020/src/main/java/hzt/aoc/day17/ConwayCubes.java
+++ advent-of-code-2020/src/main/java/hzt/aoc/day17/ConwayCubes.java
@@ -4,50 +4,53 @@ import aoc.utils.ChallengeDay;
 import org.hzt.utils.function.primitives.IntBiFunction;
 import org.hzt.utils.sequences.Sequence;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public record ConwayCubes(List<String> input) implements ChallengeDay {
 
     @Override
     public Integer part1() {
-        return solve((x, y) -> new Point3D(x, y, 0));
+        return solve((x, y) -> new Cube3D(x, y, 0));
     }
 
     @Override
     public Integer part2() {
-        return solve((x, y) -> new Point4D(x, y, 0, 0));
+        return solve((x, y) -> new Cube4D(x, y, 0, 0));
     }
 
-    private <N extends Node<N>> int solve(IntBiFunction<N> nodeSupplier) {
+    private <C extends Cube<C>> int solve(IntBiFunction<C> cubeSupplier) {
         // Simulate 6 cycles
-        return Sequence.generate(() -> simulateCycle(buildInitialState(nodeSupplier)), ConwayCubes::simulateCycle)
+        return Sequence.generate(() -> simulateCycle(buildInitialState(cubeSupplier)), ConwayCubes::simulateCycle)
                 .take(6)
                 .last()
                 .size();
     }
 
-    private <N extends Node<N>> Set<N> buildInitialState(final IntBiFunction<N> nodeSupplier) {
-        final var activeCubes = new HashSet<N>();
+    private <C extends Cube<C>> Set<C> buildInitialState(final IntBiFunction<C> cubeSupplier) {
+        final var activeCubes = new HashSet<C>();
         // Initial state (z=0)
         for (var y = 0; y < input.size(); y++) {
             final var s = input.get(y);
             for (var x = 0; x < s.length(); x++) {
                 if (s.charAt(x) == '#') {
-                    activeCubes.add(nodeSupplier.apply(x, y));
+                    activeCubes.add(cubeSupplier.apply(x, y));
                 }
             }
         }
         return activeCubes;
     }
 
-    record Point3D(int x, int y, int z) implements Node<Point3D> {
-        public List<Point3D> getNeighbors() {
-            final var neighbors = new ArrayList<Point3D>();
+    record Cube3D(int x, int y, int z) implements Cube<Cube3D> {
+        public List<Cube3D> getNeighbors() {
+            final var neighbors = new ArrayList<Cube3D>();
             for (var dx = -1; dx <= 1; dx++) {
                 for (var dy = -1; dy <= 1; dy++) {
                     for (var dz = -1; dz <= 1; dz++) {
                         if (dx == 0 && dy == 0 && dz == 0) continue;
-                        neighbors.add(new Point3D(x + dx, y + dy, z + dz));
+                        neighbors.add(new Cube3D(x + dx, y + dy, z + dz));
                     }
                 }
             }
@@ -55,15 +58,15 @@ public record ConwayCubes(List<String> input) implements ChallengeDay {
         }
     }
 
-    record Point4D(int x, int y, int z, int w) implements Node<Point4D> {
-        public List<Point4D> getNeighbors() {
-            final var neighbors = new ArrayList<Point4D>();
+    record Cube4D(int x, int y, int z, int w) implements Cube<Cube4D> {
+        public List<Cube4D> getNeighbors() {
+            final var neighbors = new ArrayList<Cube4D>();
             for (var dx = -1; dx <= 1; dx++) {
                 for (var dy = -1; dy <= 1; dy++) {
                     for (var dz = -1; dz <= 1; dz++) {
                         for (var dw = -1; dw <= 1; dw++) {
                             if (dx == 0 && dy == 0 && dz == 0 && dw == 0) continue;
-                            neighbors.add(new Point4D(x + dx, y + dy, z + dz, w + dw));
+                            neighbors.add(new Cube4D(x + dx, y + dy, z + dz, w + dw));
                         }
                     }
                 }
@@ -72,26 +75,23 @@ public record ConwayCubes(List<String> input) implements ChallengeDay {
         }
     }
 
-    interface Node<N extends Node<N>> {
-        Iterable<N> getNeighbors();
+    interface Cube<C extends Cube<C>> {
+        Iterable<C> getNeighbors();
     }
 
-    private static <N extends Node<N>> Set<N> simulateCycle(Set<N> active) {
-        final var nextActive = new HashSet<N>();
-        // Maps a node to the number of active neighbors it has
-        final var neighborCounts = new HashMap<N, Integer>();
-
-        for (final var n : active) {
-            for (final var neighbor : n.getNeighbors()) {
-                neighborCounts.put(neighbor, neighborCounts.getOrDefault(neighbor, 0) + 1);
-            }
-        }
+    private static <C extends Cube<C>> Set<C> simulateCycle(Set<C> active) {
+        // Maps a cube to the number of active neighbors it has
+        final var neighborCounts = Sequence.of(active)
+                .flatMap(Cube::getNeighbors)
+                .grouping()
+                .eachCount();
         // Apply Conway Rules
+        final var nextActive = new HashSet<C>();
         // 1. Active: stays active if 2 or 3 neighbors are active
-        for (final var n : active) {
-            final var count = neighborCounts.getOrDefault(n, 0);
+        for (final var cube : active) {
+            final var count = neighborCounts.getOrDefault(cube, 0L);
             if (count == 2 || count == 3) {
-                nextActive.add(n);
+                nextActive.add(cube);
             }
         }
         // 2. Inactive: becomes active if exactly 3 neighbors are active
